b1819e85cf23965e43fb2f74c7e908d73ed8c8b6
gilbert9172 <gilbert9172@naver.com>
2024-10-03 14:59:19 +0900

    [Problem4_feature1] - 대문자 소문자 변형해주는 로직

---
+++ src/main/java/problem4/Problem4.java
@@ -0,0 +1,49 @@
+package problem4;
+
+public class Problem4 {
+
+//    private final Map<Integer, Character> upperCase = processingAsciiArray(65, 90);
+//    private final Map<Integer, Character> lowerCase = processingAsciiArray(97, 122);
+//    private Map<Integer, Character> processingAsciiArray(int start, int end) {
+//        Map<Integer, Character> asciiMap = new HashMap<>();
+//        for (int i = start; i <= end; i++) {
+//            asciiMap.put(i - start, ((char) i));
+//        }
+//        return asciiMap;
+//    }
+
+    public String solution(String input) {
+        return convertSentence(input);
+    }
+
+
+    public String convertSentence (String sentence) {
+        StringBuilder newSentence = new StringBuilder();
+        for (int i = 0; i < sentence.length(); i++) {
+            char letter = sentence.charAt(i);
+            newSentence.append(convertLetter(letter));
+        }
+        return newSentence.toString();
+    }
+
+    private char convertLetter(char letter) {
+        if (letter >= 'A' && letter <= 'Z') {
+            return (char) ('A' + 'Z' - letter);
+        } else if (letter >= 'a' && letter <= 'z') {
+            return (char) ('a' + 'z' - letter);
+        }
+        return letter;
+    }
+
+//    private void appendLetter(char letter, StringBuilder newSentence) {
+//        if (letter >= 65 && letter <= 90) {
+//            Character newLetter = upperCase.get(Math.abs(letter - 90));
+//            newSentence.append(newLetter);
+//        } else if (letter >= 97 && letter <= 122) {
+//            Character newLetter = lowerCase.get(Math.abs(letter - 122));
+//            newSentence.append(newLetter);
+//        } else {
+//            newSentence.append(letter);
+//        }
+//    }
+}
---
+++ src/test/java/Problem4Test.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import problem4.Problem4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Problem4Test {
+
+    private final Problem4 problem4 = new Problem4();
+
+    @Test
+    void case1() {
+        String word = "I love you";
+        String result = "R olev blf";
+        assertThat(problem4.solution(word)).isEqualTo(result);
+    }
+
+    @Test
+    void case2() {
+        String word = "abcdefghijklmnopqrstuvwxyz !@#$%^&*  ";
+        String result = "zyxwvutsrqponmlkjihgfedcba !@#$%^&*  ";
+        assertThat(problem4.solution(word)).isEqualTo(result);
+    }
+
+    @Test
+    void case3() {
+        String word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !@#$%^&*  ";
+        String result = "ZYXWVUTSRQPONMLKJIHGFEDCBA !@#$%^&*  ";
+        assertThat(problem4.solution(word)).isEqualTo(result);
+    }
+}
